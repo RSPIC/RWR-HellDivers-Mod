dd5c01e64512da7a00344678b96c0c9634ec9697
RSPIC <rst1260860429@gmail.com>
2023-01-03 20:15:04 +0800

    完善武器伤害，已修改每个HD武器
    基础武器依据wiki和伤害类型写入伤害
    定义载具伤害方式，载具默认防-1.0伤害
    定义武器伤害系统，致死率默认1.0，通过后效补全实际伤害。
    提高HD通用护甲0.5抗致死，可有效减少友军伤害。敌军武器仅需提高0.5致死伤害即可修正
    修改部分武器特效

--- hell_diver/normal.bat
+++
@@ -1,4 +0,0 @@
-REM 填写rwr本体路径
-cd /d W:\Game\steamapps\common\RunningWithRifles
-REM 填写map=“启动map的路径” package=“启动mod包路径”
-rwr_game.exe map=W:\Game\steamapps\common\RunningWithRifles\media\packages\hell_diver\maps\lobby package=W:\Game\steamapps\common\RunningWithRifles\media\packages\hell_diver
